20b1617d4de0f10896b636aac4c9c30efce74551
yusk <tdfagamdb@yahoo.co.jp>
2018-11-09 18:10:56 +0900

    update

--- getho.rb
+++ getho.rb
@@ -3,7 +3,9 @@ require "formula"
 HOMEBREW_GETHO_VERSION='0.1.0'
 
 class Httr < Formula
+  url "https://github.com/popshootjapan/homebrew-getho/releases/download/#{HOMEBREW_GETHO_VERSION}/getho.tar.gz"
   homepage "https://getho.io"
+  sha256 "3cbfe48ae3db1055bfc012d8f06b367a6faaf91f1bd81555318370ee4afc5ead"
 
   version HOMEBREW_GETHO_VERSION
 
